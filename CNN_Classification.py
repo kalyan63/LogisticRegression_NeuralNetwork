f4e74210cf23d8b3da202b536cf1a6f69e9d4717
kalyan63 <skalyanreddy123@gmail.com>
2021-04-20 20:37:14 +0530

    Question 7

---
+++ CNN_Classification.py
@@ -0,0 +1,77 @@
+from matplotlib import pyplot
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
+
+#This is for VGG1
+def define_model_1():
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(200, 200, 3)))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dense(1, activation='sigmoid'))
+	# compile model
+	opt = SGD(lr=0.001, momentum=0.9)
+	model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+	return model
+
+#This is for VGG2
+def define_model_2():
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(200, 200, 3)))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dense(1, activation='sigmoid'))
+	# compile model
+	opt = SGD(lr=0.001, momentum=0.9)
+	model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+	return model
+
+# plot diagnostic learning curves
+def summarize_diagnostics(history,file_name):
+	# plot loss
+	pyplot.subplot(211)
+	pyplot.title('Cross Entropy Loss')
+	pyplot.plot(history.history['loss'], color='blue', label='train')
+	pyplot.plot(history.history['val_loss'], color='orange', label='test')
+	# plot accuracy
+	pyplot.subplot(212)
+	pyplot.title('Classification Accuracy')
+	pyplot.plot(history.history['accuracy'], color='blue', label='train')
+	pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
+	# save plot to file
+	pyplot.savefig(file_name + '_plot.png')
+	pyplot.close()
+    
+ 
+# run the test harness for evaluating a model
+def run_test_harness(model_p, f_name):
+	# define model
+	model = model_p()
+	# create data generator
+	datagen = ImageDataGenerator(rescale=1.0/255.0)
+	# prepare iterators
+	train_it = datagen.flow_from_directory('Images/Train/',
+		class_mode='binary',target_size=(200, 200))
+	test_it = datagen.flow_from_directory('Images/Test/',
+		class_mode='binary', target_size=(200, 200))
+	# fit model
+	history = model.fit(train_it, steps_per_epoch=len(train_it),
+		validation_data=test_it, validation_steps=len(test_it), epochs=20, verbose=0)
+	# evaluate model
+	_, acc = model.evaluate(test_it, steps=len(test_it), verbose=0)
+	print('> %.3f' % (acc * 100.0))
+	# learning curves
+	summarize_diagnostics(history,f_name)
+
+run_test_harness(define_model_1,'VGG1')
+run_test_harness(define_model_2,'VGG2')
---
+++ Question7.md
@@ -0,0 +1,11 @@
+## Here we use CNN to classify images of category Computer and Humans in Images folder
+
+###  The graph for the Loss and Accuracy for VGG1 and VGG2 are: 
+
+1. **VGG1 graph:**
+
+    >!['VGG1'](VGG1_plot.png)
+
+2. **VGG2 graph:**
+
+    >!['VGG2'](VGG2_plot.png)
